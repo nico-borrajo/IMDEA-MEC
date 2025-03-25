3b19fdf378bd5979fdf8cc975323163de429756d
Nico B <nico.borrajo@gmail.com>
2025-03-25 10:32:04 +0100

    MEC 2025

--- tsp/quantum.js
+++ tsp/quantum.js
@@ -29,37 +29,38 @@ new p5(s => {
     const realBestDist = realPathDist(bestPath);
 
     s.setup = function() {
-        s.createCanvas(width, height);        
+        s.createCanvas(width, height);
     }
-    
-    s.draw = function () {
+
+    s.draw = function() {
         s.background(255);
 
         if (start) {
-            s.strokeWeight(s.lerp(4, 2, quantumTime/240));
+            s.strokeWeight(s.lerp(4, 2, quantumTime / 240));
             s.stroke(45, 64, 89, 255 - quantumTime);
             // draw all paths
             for (let i = 0; i < numPoints; i++) {
-                for (let j = i+1; j < numPoints; j++) {
+                for (let j = i + 1; j < numPoints; j++) {
                     s.line(points[i].x, points[i].y, points[j].x, points[j].y);
                 }
             }
-    
+
             // draw best path
-            s.strokeWeight(s.lerp(3, 5, quantumTime/240));
+            s.strokeWeight(s.lerp(3, 5, quantumTime / 240));
             s.stroke(45, 64, 89, quantumTime);
             drawPath(bestPath, s);
-            
+
             // update leaderboard
-            update_leaderboard(s.lerp(realWorstDist, realBestDist, quantumTime/240), 'quantum');
-            
+            update_leaderboard(s.lerp(realWorstDist, realBestDist, quantumTime / 240), 'quantum');
+
             // update time
-            quantumTime = Math.min(quantumTime+1, 240);
+            quantumTime = Math.min(quantumTime + 1, 240);
         }
 
         // draw points
         s.noStroke();
         s.fill('#ff1a8c');
-        points.forEach(({x, y}) => s.circle(x, y, pointRadius*2));
+        points.forEach(({ x, y }) => s.circle(x, y, pointRadius * 2));
     }
 }, 'quantum');
+
